ea98e5cd9db1b67dc582c6b6f1666c47bbd1063b
Vladimir Morozov <mrvlmor@gmail.com>
2019-08-17 20:15:23 +0300

    тест пулреквеста

--- app/src/main/java/ru/geekbrains/myapplication/ScrollingActivity.java
+++ app/src/main/java/ru/geekbrains/myapplication/ScrollingActivity.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    // Тест пул реквеста
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
